f76873e2ae7e13970aa03344bd3648f820006f75
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2017-12-23 00:38:05 -0600

    Nuevos comandos y fixes

--- grupo_bot.py
+++ grupo_bot.py
@@ -30,18 +30,38 @@ search_browser_main_window = search_browser.current_window_handle
 time.sleep(1)
 print("Se han cargado las páginas necesarias...")
 
-def random_img(kw):
+def re_msg(el):
+    time.sleep(.25)
+    webdriver.ActionChains(browser).move_to_element(el).perform()
+    time.sleep(.25)
+    browser.execute_script('document.getElementsByClassName("jZ4tp _1i1U7")[0].firstElementChild.click()')
+    re_menu = browser.find_element_by_css_selector("._3lSL5._2dGjP._1vu-E")
+    webdriver.ActionChains(browser).move_to_element(re_menu).click(re_menu).perform()
+def random_img(kw, msg_error):
     search_browser.get("https://www.google.com.mx/search?tbm=isch&q="+kw)
-    webdriver.ActionChains(search_browser).send_keys(Keys.END).perform()
-    time.sleep(.5)
-    webdriver.ActionChains(search_browser).send_keys(Keys.END).perform()
-    time.sleep(.5)
-    webdriver.ActionChains(search_browser).send_keys(Keys.END).perform()
-    time.sleep(.5)
-    webdriver.ActionChains(search_browser).send_keys(Keys.END).perform()
-    time.sleep(.5)
-    search_browser.execute_script('document.getElementsByClassName("rg_ic rg_i")['+str(randint(0,99))+'].click(); document.getElementsByClassName("irc_fsl i3596")[1].click();')
+    for i in range(5):
+        webdriver.ActionChains(search_browser).send_keys(Keys.END).perform()
+        time.sleep(.5)
+    try:
+        search_browser.execute_script('document.getElementsByClassName("rg_ic rg_i")['+str(randint(0,399))+'].click(); document.getElementsByClassName("irc_fsl i3596")[1].click();')
+    except:
+        search_browser.switch_to_window(search_browser.window_handles[1])
+        search_browser.close()
+        search_browser.switch_to_window(search_browser_main_window)
+        send_msg(msg_error)
+        random_img(kw, msg_error)
+        return
     search_browser.switch_to_window(search_browser.window_handles[1])
+    try:
+        tag = search_browser.execute_script('return document.body.firstElementChild.tagName')
+    except:
+        tag = "undefined"
+    if(tag != "IMG"):
+        search_browser.close()
+        search_browser.switch_to_window(search_browser_main_window)
+        send_msg(msg_error)
+        random_img(kw, msg_error)
+        return
     webdriver.ActionChains(search_browser).key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL).perform()
     webdriver.ActionChains(browser).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
     time.sleep(2)
@@ -98,7 +118,7 @@ def comando(cmd, cnt):
 
     elif(cmd == bot_ext+"dic"):
         send_msg("Buscando...")
-        search_browser.get("https://www.google.com.mx/search?q="+cnt.replace(" ","+")+"+definicion")
+        search_browser.get("https://www.google.com.mx/search?q=define%3A"+cnt.replace(" ","+"))
         res = search_browser.find_elements_by_css_selector(".lr_dct_ent.vmod")
         if(res):
             send_msg( res[0].text)
@@ -122,12 +142,11 @@ def comando(cmd, cnt):
         send_msg(search_browser.execute_script('return document.getElementsByClassName("conftext")['+str(randint(0,9))+'].innerText'))
     elif(cmd == bot_ext+"loli"):
         send_msg("Ahí te va 7u7...")
-        send_msg("(Esta cuenta no se responsabiliza de los contenidos audiovisuales enviados por el bot, los contenidos han sido elegidos al azar desde Google Imagenes)")
-        random_img("lolis")
+        random_img("lolis", "Uy!... Se nos escapó tu loli.\nTe buscaremos otra.")
         send_msg("Servido papu 7u7")
     elif(cmd == bot_ext+"mywaifu"):
         send_msg("Y tu waifu es...")
-        random_img("anime waifu")
+        random_img("anime waifu", "No puede ser! Uno de tus compañeros te robó a tu waifu.\nTe estoy buscando otra que sea fiel.")
     elif(cmd == bot_ext+"dados"):
         send_msg("Lanzando...")
         send_msg(str(randint(1,6)))
@@ -136,17 +155,26 @@ def comando(cmd, cnt):
             send_msg("Por favor pregunta algo a la bola mágica...")
         else:
             send_msg(ball8_answers[randint(0,19)])
+    elif(cmd == bot_ext+"cat"):
+        send_msg("Buscando...")
+        random_img("cute cats", "Este gato es demasiado para tí.\nBuscando más gatos")
+    elif(cmd == bot_ext+"monachina3d"):
+        send_msg("Buscando monas chinas 3d...")
+        send_msg("(Sujeto a errores con monos chinos 3d :v)")
+        random_img('"ggspics" cute site:twitter.com', "Resulta que esa mona china 3d era hombre.\nBuscando otra mona china 3d.")
     elif(cmd == bot_ext+"cmd"):
-        send_msg("• "+bot_ext+"say solicitud -> El bot dirá lo que escribas. (ej. "+bot_ext+"say Soy un bot :v)")
-        send_msg("• "+bot_ext+"search solicitud -> El bot te enviará 5 links a páginas sobre lo que escribas. (ej. "+bot_ext+"search 7 razones para no suicidarme)")
-        send_msg("• "+bot_ext+"dic solicitud -> El bot buscará la definicion de diccionario sobre lo que escribas. (ej. "+bot_ext+"dic Computadora)")
-        send_msg("• "+bot_ext+"card solicitud -> El bot te mandara información de las tarjetas de Google sobre lo que escribas como bandas, lugares, personas, peliculas, repartos, etc. (ej. "+bot_ext+"card Cinemex cartelera)")
-        send_msg("• "+bot_ext+"chiste -> El bot contará un chiste.")
-        send_msg("• "+bot_ext+"conf -> El bot buscará confesiones.")
-        send_msg("• "+bot_ext+"loli -> El bot te mandará una loli 7u7.")
-        send_msg("• "+bot_ext+"dados -> El bot dará un número entre 1 y 6.")
-        send_msg("• "+bot_ext+"mywaifu -> El bot te eligirá una waifu.")
-        send_msg("• "+bot_ext+"8ball pregunta -> El bot te responderá la pregunta (preguntas sí/no) que le mandes.")
+        send_msg("• "+bot_ext+"say solicitud -> Diré lo que escribas. (ej. "+bot_ext+"say Soy un bot :v)")
+        send_msg("• "+bot_ext+"search solicitud -> Enviaré 5 links a páginas sobre lo que escribas. (ej. "+bot_ext+"search 7 razones para no suicidarme)")
+        send_msg("• "+bot_ext+"dic solicitud -> Buscaré la definicion de diccionario sobre lo que escribas. (ej. "+bot_ext+"dic Computadora)")
+        send_msg("• "+bot_ext+"card solicitud -> Mandaré información de las tarjetas de Google sobre lo que escribas como bandas, lugares, personas, peliculas, repartos, etc. (ej. "+bot_ext+"card Cinemex cartelera)")
+        send_msg("• "+bot_ext+"chiste -> Contaré un chiste.")
+        send_msg("• "+bot_ext+"conf -> Buscaré confesiones anónimas de internet.")
+        send_msg("• "+bot_ext+"loli -> Te daré una loli en adopción 7u7.")
+        send_msg("• "+bot_ext+"cat -> Te mandaré un gato bien prron.")
+        send_msg("• "+bot_ext+"dados -> Te daré un número entre 1 y 6.")
+        send_msg("• "+bot_ext+"mywaifu -> Buscaré a tu verdadera waifu 2d.")
+        send_msg("• "+bot_ext+"monachina3d -> Buscaré a una mona china 3d.")
+        send_msg("• "+bot_ext+"8ball pregunta -> Te responderé una pregunta (preguntas sí/no) que mandes.")
     else:
         send_msg( "Para ver los comandos escribe "+bot_ext+"cmd.")
 
@@ -156,13 +184,14 @@ except Exception as e:
     input("[input]     Esperando iniciar sesión en whatsapp... Enter para continuar")
 
 indice = browser.execute_script('chat = document.getElementsByClassName("chat-title");'+
-                                'for(var i = 0 ; i < chat.length ; ++i){if(chat[i].innerText == "Ingeniería en Sistemas")return i}') #+52 1 492 171 3244 Ingeniería en Sistemas
+                                'for(var i = 0 ; i < chat.length ; ++i){if(chat[i].innerText == "+52 1 492 171 3244")return i}') #+52 1 492 171 3244 Ingeniería en Sistemas
 
 try:
     grupo = browser.find_elements_by_css_selector("._2wP_Y")
     grupo[indice].click()
-    #send_msg("---- Bot iniciado ----")
-    #send_msg("Envía "+bot_ext+"cmd para ver los comandos.")
+    send_msg("---- Bot iniciado ----")
+    send_msg("(Esta cuenta no se responsabiliza de los contenidos audiovisuales enviados por el bot, los contenidos han sido elegidos al azar desde Google Imagenes)")
+    send_msg("Envía "+bot_ext+"cmd para ver los comandos.")
 except Exception as e:
     print(e)
 while (1):
@@ -178,19 +207,23 @@ while (1):
         lst_msg = "["+time.strftime("%H:%M:%S")+"]   "+msg_complete
         msg = browser.find_elements_by_css_selector(".msg")
         if(msg[browser.execute_script('return document.getElementsByClassName("msg").length-1;')].get_attribute("id") != "1" and msg_content[0]==bot_ext):
+            msg = browser.find_elements_by_css_selector(".message")
+            msg = msg[browser.execute_script('return document.getElementsByClassName("message").length-1;')]
+            re_msg(msg)
             ttl_cmd += 1;
             browser.execute_script('var el = document.getElementsByClassName("msg"); el[el.length-1].id = 1')
             split = msg_content.split(" ")
             comando(split[0], msg_content.replace(split[0]+" ", "", 1))
             lst_cmd = "["+time.strftime("%H:%M:%S")+"]   "+split[0]
             lst_cmd_c = "["+time.strftime("%H:%M:%S")+"]   "+msg_complete
+            time.sleep(2)
     except Exception as e:
-        brw_excpt = "["+time.strftime("%H:%M:%S")+"]   "+str(str(e))
+        brw_excpt = "["+time.strftime("%H:%M:%S")+"]   "+str(e)
     except:
         brw_excpt = "["+time.strftime("%H:%M:%S")+"]   [Error desconocido]"
     os.system('cls')
     try:
-        print("[WhatsApp Bot Debug Panel]")
+        print("[WhatsApp Bot Debug Panel]\n")
         print("[Total de comandos usados]    "+str(ttl_cmd))
         print("[Ultimo comando]              "+lst_cmd)
         print("[Ultimo comando completo]     "+lst_cmd_c)
